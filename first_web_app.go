62685d89d16bb7d6fd73d79c8c0a09683e88680f
Leo <lpylpyleo@gmail.com>
2019-07-26 14:15:14 +0800

    exer

--- first_web_app.go
+++ first_web_app.go
@@ -4,12 +4,31 @@ import (
   "fmt"
   "net/http"
   "log"
+  "time"
+  "strconv"
 )
+
+type Student struct {
+  name string
+  age int
+}
+
+func (a Student) String() string {
+  return a.name + ", " + strconv.Itoa(a.age)
+}
+
+var A Student
+
+func init(){
+  A = Student{name:"tom",age:20}
+}
 
 func sayHello(w http.ResponseWriter, r *http.Request){
   r.ParseForm()
   fmt.Println(r.Form)
-  fmt.Fprintf(w, "Hello")
+  fmt.Fprintf(w, time.Now().String())
+  fmt.Fprintf(w, "\n")
+  fmt.Fprintf(w, A.String())
 }
 
 func main(){
